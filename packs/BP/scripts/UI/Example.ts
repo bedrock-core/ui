1783d69fede2f2f855f573961eb496aca1c30d19
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-09-25 00:45:44 +0200

    Refactor core UI components and properties
    - Updated panel component to use "controled_layout" instead of "control".
    - Enhanced control properties to include a "type" field and reorganized bindings for clarity.
    - Modified layout properties to use pixel values for width and height, and added corresponding bindings.
    - Changed server form to reference the new text component instead of the panel.
    - Introduced new label component for future enhancements.
    - Added a comprehensive text component with detailed bindings for various text properties and attributes.

--- packs/BP/scripts/UI/Example.ts
+++ packs/BP/scripts/UI/Example.ts
@@ -1,26 +1,34 @@
-import { Component, Panel, Text } from '@bedrock-core/ui';
+import { Component, Text } from '@bedrock-core/ui';
 
 export function ExampleComponent(): Component {
-  return Panel({
-    height: '500',
-    width: '2000',
-    x: '0',
-    y: '0',
-    children: [
-      Text({
-        height: '40',
-        width: '100',
-        x: '100',
-        y: '40',
-        value: 'Player Settings',
-      }),
-      Text({
-        height: '40',
-        width: '100',
-        x: '100',
-        y: '90',
-        value: 'Player Settings 2',
-      }),
-    ],
+  return Text({
+    height: '40',
+    width: '100',
+    x: '100',
+    y: '40',
+    value: 'Player Settings',
   });
+
+  // return Panel({
+  //   height: '500',
+  //   width: '2000',
+  //   x: '0',
+  //   y: '0',
+  //   children: [
+  //     Text({
+  //       height: '40',
+  //       width: '100',
+  //       x: '100',
+  //       y: '40',
+  //       value: 'Player Settings',
+  //     }),
+  //     Text({
+  //       height: '40',
+  //       width: '100',
+  //       x: '100',
+  //       y: '90',
+  //       value: 'Player Settings 2',
+  //     }),
+  //   ],
+  // });
 }
