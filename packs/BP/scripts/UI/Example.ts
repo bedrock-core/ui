6705077359974e1389306ecaeec3d4bbd69eab14
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-09-25 00:25:58 +0200

    Refactor UI components and update bindings for improved functionality

--- packs/BP/scripts/UI/Example.ts
+++ packs/BP/scripts/UI/Example.ts
@@ -1,25 +1,26 @@
-import { Component, Functional, Input, Panel } from '@bedrock-core/ui';
+import { Component, Panel, Text } from '@bedrock-core/ui';
 
-export function Example(): Functional<Component> {
+export function ExampleComponent(): Component {
   return Panel({
-    display: 'flex',
-    orientation: 'vertical',
-  }, [
-    Input({
-      label: 'Name',
-      maxLength: 5,
-      placeholder: 'Enter your name',
-      value: 'yes',
-      textType: 'ExtendedASCII',
-      multiline: true,
-      width: 13,
-      height: 335,
-    }),
-    Input({
-      label: 'Description',
-      multiline: true,
-      width: 56677,
-      height: 900,
-    }),
-  ]);
+    height: '500',
+    width: '2000',
+    x: '0',
+    y: '0',
+    children: [
+      Text({
+        height: '40',
+        width: '100',
+        x: '100',
+        y: '40',
+        value: 'Player Settings',
+      }),
+      Text({
+        height: '40',
+        width: '100',
+        x: '100',
+        y: '90',
+        value: 'Player Settings 2',
+      }),
+    ],
+  });
 }
--- packs/BP/scripts/main.ts
+++ packs/BP/scripts/main.ts
@@ -1,16 +1,12 @@
 import { present } from '@bedrock-core/ui';
 import { ButtonPushAfterEvent, Player, world } from '@minecraft/server';
-import { ModalFormData } from '@minecraft/server-ui';
 import { MinecraftEntityTypes } from '@minecraft/vanilla-data';
-import { Example } from './UI/Example';
+import { ExampleComponent } from './UI/Example';
 
 world.afterEvents.buttonPush.subscribe(({ source }: ButtonPushAfterEvent): void => {
   if (source.typeId === MinecraftEntityTypes.Player) {
-    const player = source as Player;
-
-    const form = new ModalFormData();
-
-    present(form, player, Example());
+    // @ts-expect-error nested linked repo issue
+    present(source as Player, ExampleComponent());
   }
 });
 
