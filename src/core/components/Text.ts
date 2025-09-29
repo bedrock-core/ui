ff8d14657f58dc95183e9ea7d32e15c9ceb1146d
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-09-30 00:59:53 +0200

    removed localize as we will default to true always, removed color and locked color, prefer to use color codes

--- src/core/components/Text.ts
+++ src/core/components/Text.ts
@@ -6,13 +6,10 @@ import { Logger } from '../../util/Logger';
 import { serialize, serializeString } from '../serializer';
 
 export interface TextStyle {
-  color?: [number, number, number];
-  lockedColor?: [number, number, number];
   shadow?: boolean;
   fontSize?: 'small' | 'normal' | 'large' | 'extra_large';
   fontScaleFactor?: number;
   fontType?: 'default' | 'unicode' | 'smooth';
-  localize?: boolean;
   textAlignment?: 'left' | 'center' | 'right';
 }
 
@@ -28,17 +25,10 @@ export function Text({ value, textStyle, ...rest }: TextProps): Component {
         // Core identity
         type: serializeString('text'),
         // Properties
-        colorR: textStyle?.color?.[0] ?? 1,
-        colorG: textStyle?.color?.[1] ?? 1,
-        colorB: textStyle?.color?.[2] ?? 1,
-        lockedColorR: textStyle?.lockedColor?.[0] ?? 1,
-        lockedColorG: textStyle?.lockedColor?.[1] ?? 1,
-        lockedColorB: textStyle?.lockedColor?.[2] ?? 1,
         shadow: textStyle?.shadow ?? false,
         fontSize: serializeString(textStyle?.fontSize ?? 'normal'),
         fontScaleFactor: textStyle?.fontScaleFactor ?? 1,
         fontType: serializeString(textStyle?.fontType ?? 'default'),
-        localize: textStyle?.localize ?? false,
         textAlignment: serializeString(textStyle?.textAlignment ?? 'left'),
         text: serializeString(value ?? ''),
         ...rest,
