3c2cbe330f0ed0f21424077d4333ebf6dfbcceef
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-09-29 04:14:12 +0200

    panel works, text works partially

--- packs/BP/scripts/UI/Example.ts
+++ packs/BP/scripts/UI/Example.ts
@@ -1,4 +1,4 @@
-import { Component, Panel } from '@bedrock-core/ui';
+import { Component, Panel, Text } from '@bedrock-core/ui';
 
 export function ExampleComponent(): Component {
   return Panel({
@@ -7,6 +7,22 @@ export function ExampleComponent(): Component {
     x: 10,
     y: 10,
     layer: 100,
-    children: [],
+    children: [
+      Panel({
+        width: 200,
+        height: 20,
+        x: 40,
+        y: 10,
+        layer: 100,
+        children: [],
+      }),
+      Text({
+        width: 200,
+        height: 20,
+        x: 40,
+        y: 10,
+        value: 'Hello World',
+      }),
+    ],
   });
 }
