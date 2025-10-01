df1a93c0af2db20f6f017dd9943a4d98c717b5e6
DrAv0011 <43525295+drav0011@users.noreply.github.com>
2025-10-01 02:38:39 +0200

    Refactor UI components: Update Example function, add Fragment support, and include logo image

--- packs/BP/scripts/UI/Example.ts
+++ packs/BP/scripts/UI/Example.ts
@@ -1,56 +1,28 @@
-import { Component, Panel, Text, Image } from '@bedrock-core/ui';
+import { Component, Panel, Text, Image, Fragment } from '@bedrock-core/ui';
 
-export function ExampleComponent(): Component {
-  return Panel({
-    width: 100,
-    height: 250,
-    x: 10,
-    y: 10,
-    children: [
+export function Example(): Component {
+  return Fragment({
+    width: 854, height: 480, x: 0, y: 0, children: [
+      // Center panel with logo
       Panel({
-        width: 200,
-        height: 20,
-        x: 50,
-        y: 15,
-        children: [],
-      }),
-      Panel({
-        width: 200,
-        height: 20,
-        x: 40,
-        y: 10,
-        children: [
-          Text({
-            width: 200,
-            height: 20,
-            x: 400,
-            y: 40,
-            value: 'server only custom ui!!!!',
-          }),
-          Text({
-            width: 200,
-            height: 20,
-            x: 400,
-            y: 80,
-            value: 'server only custom ui!!!!',
-          }),
-          Image({
-            width: 20,
-            height: 20,
-            x: 300,
-            y: 40,
-            enabled: false,
-            texture: 'textures/items/apple',
-          }),
+        width: 200, height: 36, x: 327, y: 222, children: [
+          Image({ width: 200, height: 36, x: 327, y: 222, texture: 'textures/ui/core-ui/logo' }),
         ],
       }),
-      Text({
-        width: 200,
-        height: 20,
-        x: 40,
-        y: 10,
-        value: 'Hello World',
+
+      // Right panel with text
+      Panel({
+        width: 100, height: 250, x: 580, y: 75, children: [
+          Text({ width: 180, height: 20, x: 585, y: 80, value: 'Right Side Text 1' }),
+          Text({ width: 180, height: 20, x: 585, y: 110, value: 'Right Side Text 2' }),
+        ],
       }),
+
+      // Image in top left corner
+      Image({ width: 32, height: 32, x: 10, y: 10, texture: 'textures/items/apple' }),
+
+      // Text in bottom right corner
+      Text({ width: 150, height: 20, x: 694, y: 450, value: '§l§gCustom text!' }),
     ],
   });
 }
--- packs/BP/scripts/main.ts
+++ packs/BP/scripts/main.ts
@@ -1,24 +1,24 @@
 import { present } from '@bedrock-core/ui';
-import { ButtonPushAfterEvent, Player, world } from '@minecraft/server';
+import { ButtonPushAfterEvent, Player, ScriptEventCommandMessageAfterEvent, system, world } from '@minecraft/server';
 import { MinecraftBlockTypes, MinecraftEntityTypes } from '@minecraft/vanilla-data';
-import { ExampleComponent } from './UI/Example';
+import { Example } from './UI/Example';
 import { ModalFormData } from '@minecraft/server-ui';
 
 world.afterEvents.buttonPush.subscribe(({ source, block }: ButtonPushAfterEvent): void => {
   if (source.typeId === MinecraftEntityTypes.Player) {
     if (block.typeId === MinecraftBlockTypes.StoneButton) {
-      // @ts-expect-error nested linked repo issue
-      present(source as Player, ExampleComponent());
+      present(source as Player, Example());
     }
     if (block.typeId === MinecraftBlockTypes.AcaciaButton) {
       const form = new ModalFormData();
 
       form.title('test');
       form.label('test');
-
-      // @ts-expect-error nested linked repo issue
       form.show(source as Player);
     }
   }
 });
 
+system.afterEvents.scriptEventReceive.subscribe(({ sourceEntity }: ScriptEventCommandMessageAfterEvent) => {
+  present(sourceEntity as Player, Example());
+});
